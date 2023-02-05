1378cb4a1f883031e150b61e3daf1e3435c73e47
Josuex <Josuex@DESKTOP-6E8350H>
2023-02-05 15:03:58 -0600

    tarea

---
+++ src/test/java/tarea1web/Tarea1webApplicationTests.java
@@ -0,0 +1,13 @@
+package tarea1web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea1webApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
